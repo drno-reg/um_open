11b2eeb4a650d60958ac739ba548f84b120a88fb
admin <drno>
2017-12-18 15:19:52 +0300

    добавил позможность вывода информации по имени группы, hostid

--- templates/Integration/Monitoring_Systems/Zabbix/Zabbix_API.py
+++ templates/Integration/Monitoring_Systems/Zabbix/Zabbix_API.py
@@ -6,7 +6,6 @@ import requests
 import contextlib
 import simplejson as json
 
-
 # загружаем конфиг
 # пример конфига
 # URL=https://server_zabbix/api_jsonrpc.php
@@ -150,10 +149,127 @@ with no_ssl_verification():
     GET_request.encoding = 'utf-8';
     # сохранение результата в JSON
     result_hosts=GET_request.json().get("result")
-    # print(result_hosts)
-    print(GET_request.json())
+    print(result_hosts)
+    # перегоняем значания id и name хостов в словарь для того, чтобы появилась возможность по hostid itemsid
+    hostnames={}
+    for x in range(0, len(result_hosts)):
+        hostnames[result_hosts[x].get("hostid")] = result_hosts[x].get("host")
+
+    # print("Hostname по id: ", hostnames.get("38464"))
+
+    print(hostnames)
+
+    # print(d.get("38203"))
+    # print(GET_request.json())
     # считаем количество хостов
     print("Нашел хостов: ",len(result_hosts))
 
+    zabbix_get=\
+    {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": "extend",
+            "hostids": connection.get("HostIDs"),
+            # "host": "server01" только по одному имени, списком только hostid
+            "search": {
+                # "key_": "vfs.fs.size"
+                "description": "Имя ФС: /data"
+            },
+            "sortfield": "name"
+        },
+        "auth": authToken.get("result"),
+        "id": authToken.get("id")
+    }
+    #пример запроса поиска значений по конкретному полю - в данном случае по ключу
+    zabbix_get= \
+        {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "output": "extend",
+                "host": connection.get("HostName"),
+                # "host": "server01" только по одному имени, списком только hostid
+                "search": {
+                    "key_": 'vfs.fs.size[/data,free]'
+                    # "description": "Имя ФС: /data"
+                },
+                "sortfield": "name"
+            },
+            "auth": authToken.get("result"),
+            "id": authToken.get("id")
+        }
+
+    hostids=connection.get("HostIDs").split(",")
+    # print(hostids)
+
+    zabbix_get= \
+        {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "output": "extend",
+                # "host": connection.get("HostName"),
+                "hostids": hostids,
+                # "host": "server01" только по одному имени, списком только hostid
+                # "search": {
+                    # "itemid": '3294956',
+                    # "key_": "vfs.fs.size[/data,free]",
+                    # "key_": 'vfs.fs.size[/data,free]'
+                    # "description": "Имя ФС: /data"
+                # },
+            "sortfield": "name"
+        },
+        #     "method": "history.get",
+        #         "params": {
+        #             "output": "extend",
+        #             "history": 0,
+        #             "itemids": "3294956",
+        #             "sortfield": "clock",
+        #             "sortorder": "DESC",
+        #             "limit": 10
+        #         },
+            "auth": authToken.get("result"),
+            "id": authToken.get("id")
+        }
+
+    zabbix_get= \
+        {
+            "jsonrpc": "2.0",
+            "method": "item.get",
+            "params": {
+                "output": "extend",
+                "hostids": hostids,
+                # "search": {
+                #     "key_": "system"
+                # },
+                "sortfield": "name"
+            },
+            "auth": authToken.get("result"),
+            "id": authToken.get("id")
+        }
+
+    GET_request = requests.get(connection.get("URL"), data=json.dumps(zabbix_get), headers=headers);
+    GET_request.encoding = 'utf-8';
+    # сохранение результата в JSON
+    result_items_by_hostsid=GET_request.json().get("result")
+    # print(result_hosts)
+    print(result_items_by_hostsid)
+    # print("Значение: ", result_items_by_hostsid[0].get("lastvalue"))
+    # считаем количество хостов
+    # print("Нашел хостов: ",len(result_hosts))
+    # получаем информацию о всех метриках если значение  Keys отсутствует или равно * если указан фильтр то будут возвращаться только необходимые значения
+    print("Нашел метрик: ", len(result_items_by_hostsid))
+    result=[]
+    for y in range(0, len(hostids)):
+        # print(hostids[y])
+        for x in range(0, len(result_items_by_hostsid)):
+            if (result_items_by_hostsid[x].get("hostid").find(hostids[y])!=-1):
+                # print(result_items_by_hostsid[x].get("hostid")," равно ли ",hostids[y])
+                if (connection.get("Keys")!="*"):
+                    if (connection.get("Keys").find(result_items_by_hostsid[x].get("key_"))!=-1):
+                       print("hostname: \"",hostnames.get(hostids[y]), "\", "+result_items_by_hostsid[x].get("key_"), ": \"",result_items_by_hostsid[x].get("lastvalue"), "\"")
+                else:
+                    print("hostname: \"",hostnames.get(hostids[y]), "\", "+result_items_by_hostsid[x].get("key_"), ": \"",result_items_by_hostsid[x].get("lastvalue"), "\"")
 
 
