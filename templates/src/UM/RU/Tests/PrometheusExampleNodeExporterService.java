0a02b0d708f9f59dbe27372f95fc19ace942c345
drno <drno-reg@mail.ru>
2018-10-08 14:47:22 +0300

    add ready timer

--- templates/src/UM/RU/Tests/PrometheusExampleNodeExporterService.java
+++ templates/src/UM/RU/Tests/PrometheusExampleNodeExporterService.java
@@ -2,6 +2,12 @@ package UM.RU.Tests;
 
 
 import io.prometheus.client.Gauge;
+import io.prometheus.client.vertx.MetricsHandler;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 // Main application class
 public class PrometheusExampleNodeExporterService {
@@ -10,19 +16,25 @@ public class PrometheusExampleNodeExporterService {
 
     public static  void main(String[] args) throws Exception {
 
+        final Timer time = new Timer();
+        final Vertx vertx = Vertx.vertx();
+        final Router router = Router.router(vertx);
 
-//        Server server = new Server(1234);
-//        ServletContextHandler context = new ServletContextHandler();
-//        context.setContextPath("/");
-//        server.setHandler(context);
-//        context.addServlet(new ServletHolder(new MetricsServlet()), "/metrics");
-//        g.set(1);
-//        c.inc(2);
-//        s.observe(3);
-//        h.observe(4);
-//        l.labels("foo").inc(5);
-//        server.start();
-//        server.join();
+        router.route("/metrics").handler(new MetricsHandler());
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(8001);
+
+        time.schedule(new TimerTask() {
+            int i = 0;
+
+            @Override
+            public void run() { //ПЕРЕЗАГРУЖАЕМ МЕТОД RUN В КОТОРОМ ДЕЛАЕТЕ ТО ЧТО ВАМ НАДО
+
+                l.labels("foo").inc(Math.random()*3 );
+            System.out.println("4 second ago");
+        }
+    }, 0, 4000); //(4000 - ПОДОЖДАТЬ ПЕРЕД НАЧАЛОМ В МИЛИСЕК, ПОВТОРЯТСЯ 4 СЕКУНДЫ (1 СЕК = 1000 МИЛИСЕК))
+
 
     }
 }
